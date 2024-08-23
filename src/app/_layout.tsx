f67f23693c9fb9fe4c33cf84a9164839e4052f9c
juaneliasjabib02 <juaneliasjabib02@gmail.com>
2024-08-23 01:28:07 -0500

    eslint configurated

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,7 +2,17 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#f4511e",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
       <Stack.Screen name="index" />
     </Stack>
   );
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,9 @@
-import { Text, View } from 'react-native';
+import { Text, View } from "react-native";
 
 export default function Index() {
-	return (
-		<View className='h-full bg-blue-500'>
-			<Text className='text-red-500'></Text>
-		</View>
-	);
+  return (
+    <View className="h-full bg-blue-500">
+      <Text className="text-red-500 text-lg font-">NATIVE</Text>
+    </View>
+  );
 }

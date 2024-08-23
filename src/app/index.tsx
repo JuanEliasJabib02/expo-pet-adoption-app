64c2a0795d36a9fe6e583144aaa750216b7309a7
juaneliasjabib02 <juaneliasjabib02@gmail.com>
2024-08-23 00:10:54 -0500

    fix typescript error, install nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from 'react-native';
 
 export default function Index() {
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: 'center',
-				alignItems: 'center',
-			}}
-		>
-			<Text>App is working</Text>
+		<View className='h-full bg-blue-500'>
+			<Text className='text-red-500'></Text>
 		</View>
 	);
 }

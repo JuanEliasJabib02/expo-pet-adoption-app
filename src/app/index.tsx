955bd10c957d689f3674c0efa3e3da200beb9dcb
juaneliasjabib02 <juaneliasjabib02@gmail.com>
2024-08-24 23:04:05 -0500

    update

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,12 @@
-import { View } from "react-native";
-import ExampleComponent from "../lib/components/example-component";
-import config from "../lib/config/config";
+import { View } from 'react-native';
+import ExampleComponent from '../lib/components/example-component';
+import config from '../lib/config/config';
 
 export default function App() {
-  console.log(config.url_api_backend, "is-prod");
-  return (
-    <View className="h-full bg-blue-500">
-      <ExampleComponent />
-    </View>
-  );
+	console.log(config.url_api_backend, 'is-prod');
+	return (
+		<View className='h-full bg-blue-500'>
+			<ExampleComponent />
+		</View>
+	);
 }
